33ff65aa6781a2154b96721b4f3ef495174737c0
8ram <bram.boeckx.bb@gmail.com>
2018-04-13 21:24:36 +0200

    cleaning

--- sneckepp v0.2.py
+++ sneckepp v0.2.py
@@ -5,10 +5,10 @@ import xlrd
 workbook = xlrd.open_workbook("vleesje.xlsx")
 vleesjes = workbook.sheet_by_name("Blad1")
 
-column = 0 # or whatever column you want to select from
+column = 0 
 
-startRow = 1
-endRow = 25
+startRow = 1  #rij van het eerste vleesje
+endRow = 25 #rij van het laatste vleesje
 row = random.randint(startRow, endRow)
 print ("het vleesje is: {0}".format(vleesjes.cell(row, column).value))
     
